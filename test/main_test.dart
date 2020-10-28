18d1e5fe0262d1c3c639e59b6a8b94a30d3f575e
Rio Cheng <riocheng@Rios-MacBook-Air.local>
2020-10-28 19:34:24 +0700

    test

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,9 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:messio_app/main.dart';
-import 'package:messio_app/pages/ConversationPageList.dart';
-
 void main() {
   testWidgets('Main UI Test', (WidgetTester tester) async {
-    
+
   });
 }
